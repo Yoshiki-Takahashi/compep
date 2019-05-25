d20ee3999766c6452de6cb224a811126456e85c7
Yoshiki Takahashi <yoshikta@yahoo-corp.jp>
2019-05-26 00:17:13 +0900

    add struct P

--- template/main.cpp
+++ template/main.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 #define RANGE(i,n) for(int i=0;i<n;i++)
 typedef long long ll;
+struct P{
+    ll var1;
+    ll var2;
+};
+bool operator< ( const P &one, const P &two){
+    return one.var1 < two.var1;
+};
+bool operator> ( const P &one, const P &two){
+    return one.var1 > two.var1;
+};
 
 using namespace std;
 
